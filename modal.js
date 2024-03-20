427f760403c1852fc1b139ba82e468afc341d877
Yuiko Morooka <yuikomorooka@YunoMacBook-Pro.local>
2024-03-21 00:28:35 +0900

    add-modal

---
+++ modal.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('#qr-code')
+
+button.addEventListener('click', function() {
+  var buttonId = this.getAttribute('id');
+  document.getElementById('modal-container').removeAttribute('class');
+  document.getElementById('modal-container').classList.add(buttonId);
+  document.body.classList.add('modal-active');
+});
+
+ document.getElementById('modal-container').addEventListener('click', function() {
+    this.classList.add('out');
+    document.body.classList.remove('modal-active');
+  });
+  
